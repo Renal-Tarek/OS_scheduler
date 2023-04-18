d5f62aa8922a237ea817826638e00bdacf6d4ac3
Renal-Tarek <104008582+Renal-Tarek@users.noreply.github.com>
2023-04-19 00:35:20 +0200

    update in this class

--- src/Process.java
+++ src/Process.java
@@ -9,61 +9,82 @@ package os;
  *
  * @author renal
  */
-public class Process implements Comparable<Process> {
-    
-    private int process_number;
-    private float arrival_time;
-    private float burst_time;
-    private float remaining_burst_time;
-    private int process_priority;
+public class Process  implements Comparable<Process>
+{
+    private String prosess_name;
+    private int arriving_time;
+    private int remaining_burst_time;
+    private int burst_time;
+    private int priority;
+    private int waiting_time;
     private float finish_time;
-    private float waiting_time;
     private float turnAround_time;
     private static int number_of_process;
     private static int compare_type;
    
-
-
-    public Process(int process_number, float arrival_time, float burst_time) {
-        this.process_number = process_number;
-        this.arrival_time = arrival_time;
+    public Process(String process_name, int arriving_time, int burst_time) {
+        this.prosess_name= process_name;
+        this.arriving_time = arriving_time;
         this.burst_time = burst_time;
         this.remaining_burst_time = burst_time;
         number_of_process++;
     }
-
-    public Process(int process_number, int arrival_time, int burst_time, int process_priority) {
-        this(process_number, arrival_time, burst_time);
-        this.process_priority = process_priority;
-    }
-    
-    
-    public void setProcess_number(int process_number) {
-        this.process_number = process_number;
+    public Process(String prosess_name, int arrival_time, int burst_time, int priority) {
+        this(prosess_name, arrival_time, burst_time);
+        this.priority =priority;
     }
 
-    public void setArrival_time(float arrival_time) {
-        this.arrival_time = arrival_time;
+    public String getProsess_name() {
+        return prosess_name;
     }
 
-    public void setBurst_time(float burst_time) {
-        this.burst_time = burst_time;
+    public void setProsess_name(String prosess_name) {
+        this.prosess_name = prosess_name;
     }
 
-    public void setRemaining_burst_time(float remaining_burst_time) {
+    public int getArriving_time() {
+        return arriving_time;
+    }
+
+    public void setArriving_time(int arriving_time) {
+        this.arriving_time = arriving_time;
+    }
+
+    public int getRemaining_burst_time() {
+        return remaining_burst_time;
+    }
+
+    public void setRemaining_burst_time(int remaining_burst_time) {
         this.remaining_burst_time = remaining_burst_time;
     }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+    
+    public int getWaiting_time() {
+        return waiting_time;
+    }
+
+    public void setWaiting_time(int waiting_time) {
+        this.waiting_time = waiting_time;
+    } 
+
+    public void setBurst_time(int burst_time) {
+        this.burst_time = burst_time;
+    }
+    
+    public float getBurst_time() {
+        return burst_time;
+    }
+
     
     public void setFinish_time(float finish_time) {
         this.finish_time = finish_time;
-    }
-    
-    public void setProcess_priority(int process_priority) {
-        this.process_priority = process_priority;
-    }
-
-    public void setWaiting_time(float waiting_time) {
-        this.waiting_time = waiting_time;
     }
 
     public void setTurnAround_time(float turnAround_time) {
@@ -73,37 +94,12 @@ public class Process implements Comparable<Process> {
     public static void setCompare_type(int compare_type) {
         Process.compare_type = compare_type;
     }
-   
-    
-    public int getProcess_number() {
-        return process_number;
-    }
-
-    public float getArrival_time() {
-        return arrival_time;
-    }
-
-    public float getBurst_time() {
-        return burst_time;
-    }
-
-    public float getRemaining_burst_time() {
-        return remaining_burst_time;
-    }
-    
+ 
     public float getFinish_time() {
         return finish_time;
     }
     
-    public int getProcess_priority() {
-        return process_priority;
-    }
-
-    public float getWaiting_time() {
-        return waiting_time;
-    }
-
-    public float getTurnAround_time() {
+     public float getTurnAround_time() {
         return turnAround_time;
     }
     
@@ -120,7 +116,7 @@ public class Process implements Comparable<Process> {
     {
         // compare with respect to arrival time ascendengly
         if(compare_type == 0)
-            return (int)(this.getArrival_time() - p.getArrival_time());
+            return (int)(this.getArriving_time() - p.getArriving_time());
         
         // compare with respect to burst time ascendengly
         else if(compare_type == 1)
@@ -130,8 +126,9 @@ public class Process implements Comparable<Process> {
 //        else if(compare_type1==0 && compare_type1==0)
             
         else 
-            return (int)(this.getProcess_priority() - p.getProcess_priority()); 
+            return (int)(this.getPriority() - p.getPriority()); 
     }
     
 
+    
 }
